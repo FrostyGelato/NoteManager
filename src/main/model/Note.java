9959d67794b490fc37ba7f7c9f628c4ad06e1941
FrostyGelato <59901139+FrostyGelato@users.noreply.github.com>
2022-10-08 16:20:36 -0700

    added tests for Note

--- src/main/model/Note.java
+++ src/main/model/Note.java
@@ -5,13 +5,13 @@ import java.nio.file.Path;
 // Represents a note that is linked to a file in the filesystem
 public class Note {
     private String name;
-    private String fileType;
     private Path fileLocation;
-    //private int id;
     private Status status;
+    //private String fileType;
+    //private int id;
 
     //REQUIRES: a file with a valid path
-    //EFFECTS: constructs a note with a given name of file, type of file, location of file, and incomplete status
+    //EFFECTS: constructs a note with a given name of file, location of file, and incomplete status
     public Note(Path file) {
 
     }
@@ -21,23 +21,29 @@ public class Note {
 
     }
 
+    //MODIFIES: this
+    //EFFECTS: changes this status to given status
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
     public String getName() {
         return "";
     }
-
-    public String getFileType() {
-        return "";
-    }
-
+    
     public Path getFileLocation() {
         return null;
+    }
+
+    public Status getStatus() {
+        return Status.INCOMPLETE;
     }
 
     /*public int getId() {
         return 0;
     }*/
 
-    public Status getStatus() {
-        return Status.INCOMPLETE;
-    }
+    /*public String getFileType() {
+        return "";
+    }*/
 }
--- src/test/model/NoteTest.java
+++ src/test/model/NoteTest.java
@@ -1,5 +1,49 @@
 package model;
 
-class NoteTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NoteTest {
+    private Note testNote;
+    private Path testFilePath;
+
+    @TempDir
+    Path tempDir;
+
+    @BeforeEach
+    void setup() {
+        try {
+            testFilePath = tempDir.resolve( "Vocabulary.txt" );
+        }
+        catch( InvalidPathException ipe ) {
+            System.err.println("could not create test file");
+        }
+
+        testNote = new Note(testFilePath);
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("Vocabulary", testNote.getName());
+        assertEquals(testFilePath, testNote.getFileLocation());
+        assertEquals(Status.INCOMPLETE, testNote.getStatus());
+    }
+
+    @Test
+    void testSetStatusChange() {
+        testNote.setStatus(Status.NEED_REVISION);
+        assertEquals(Status.NEED_REVISION, testNote.getStatus());
+    }
+
+    @Test
+    void testSetStatusNoChange() {
+        testNote.setStatus(Status.INCOMPLETE);
+        assertEquals(Status.INCOMPLETE, testNote.getStatus());
+    }
 }
