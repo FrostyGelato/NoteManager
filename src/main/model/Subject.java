918c3da196b3101c058932d70294272f600cad81
FrostyGelato <59901139+FrostyGelato@users.noreply.github.com>
2022-10-08 16:24:34 -0700

    implemented Subject

--- src/main/model/Subject.java
+++ src/main/model/Subject.java
@@ -10,26 +10,27 @@ public class Subject {
     //REQUIRES: name is non-empty string
     //EFFECTS: constructs a subject with given name and empty list of topics
     public Subject(String name) {
-
+        this.name = name;
+        listOfTopics = new LinkedHashSet<>();
     }
 
     //MODIFIES: this
     //EFFECTS: adds the given topic to list of topics unless it's already there, in which case do nothing
     public void addTopic(Topic topic) {
-
+        listOfTopics.add(topic);
     }
 
     //MODIFIES: this
     //EFFECTS: if the given topic is in list of topics, removes it; otherwise, do nothing
     public void removeTopic(Topic topic) {
-
+        listOfTopics.remove(topic);
     }
 
     public String getName() {
-        return "";
+        return name;
     }
 
     public LinkedHashSet<Topic> getListOfTopics() {
-        return new LinkedHashSet<Topic>();
+        return listOfTopics;
     }
 }
