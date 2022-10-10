c2191575c4b643e178971da9c31b7110af1bcaa2
FrostyGelato <59901139+FrostyGelato@users.noreply.github.com>
2022-10-09 17:35:43 -0700

    added more ui elements

--- src/main/ui/NoteManager.java
+++ src/main/ui/NoteManager.java
@@ -1,6 +1,7 @@
 package ui;
 
 import model.Subject;
+import model.Topic;
 
 import java.util.LinkedHashSet;
 import java.util.Scanner;
@@ -24,13 +25,13 @@ public class NoteManager {
         init();
 
         while (keepGoing) {
-            displayMainMenu();
+            displaySubjectMenu();
             userResponse = input.next().toLowerCase();
 
             if (userResponse.equals("q")) {
                 keepGoing = false;
             } else {
-                subjectSelector(userResponse);
+                processSubjectMenuInput(userResponse);
             }
         }
 
@@ -38,13 +39,14 @@ public class NoteManager {
     }
 
     //MODIFIES: this
-    //EFFECTS: initialize input
+    //EFFECTS: initialize input and list of subjects
     private void init() {
         input = new Scanner(System.in);
+        listOfSubjects = new LinkedHashSet<>();
     }
 
     //EFFECTS: displays a list of subjects and other options
-    private void displayMainMenu() {
+    private void displaySubjectMenu() {
         System.out.println("Note Manager");
         System.out.println("Subject List:");
 
@@ -62,16 +64,65 @@ public class NoteManager {
         System.out.println("q -> quit");
     }
 
-    private void subjectSelector(String userResponse) {
+    private void processSubjectMenuInput(String userResponse) {
         if (userResponse.equals("n")) {
-            // createNewSubject();
+            createNewSubject();
         } else {
             for (Subject s: listOfSubjects) {
                 if (userResponse.equals(s.getName())) {
+
+                    displayTopicMenu(s);
+
                     break;
-                    // displayTopicsInSubject();
                 }
             }
         }
+    }
+
+    //EFFECTS: displays a list of topics in selected subject
+    private void displayTopicMenu(Subject selectedSubject) {
+
+        System.out.println("Topic List:");
+
+        if (selectedSubject.getListOfTopics().isEmpty()) {
+            System.out.println("You have no topics.");
+        } else {
+            for (Topic t: selectedSubject.getListOfTopics()) {
+                System.out.println(t.getName());
+            }
+        }
+
+        System.out.println("Enter the name of the topic you wish to open\n"
+                + "Or select from the following options:");
+        System.out.println("n -> create new topic");
+        System.out.println("s -> return to subject menu");
+    }
+
+    //MODIFIES: this
+    //EFFECTS: asks user for subject name and creates subject with given name in list
+    private void createNewSubject() {
+        System.out.println("Enter name for subject and click Enter:");
+        String subjectName = input.next();
+
+        if (isDuplicateSubject(subjectName)) {
+            System.out.println("Error: You cannot have subjects with the same name.\n"
+                    + "Please try again.");
+        } else {
+            listOfSubjects.add(new Subject(subjectName));
+        }
+    }
+
+    //REQUIRES: subjectName is non-empty
+    //EFFECTS: return true is list already contains a subject with same name; otherwise return false
+    public boolean isDuplicateSubject(String subjectName) {
+        boolean isDuplicate = false;
+
+        for (Subject s: listOfSubjects) {
+            if (subjectName.equals(s.getName())) {
+                isDuplicate = true;
+            }
+        }
+
+        return isDuplicate;
     }
 }
