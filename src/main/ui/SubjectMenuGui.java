e99cdfaaf209f0e4842396ce1bf1b61a5d66cc37
FrostyGelato <59901139+FrostyGelato@users.noreply.github.com>
2022-11-10 17:13:43 -0800

    added basic subject gui

--- src/main/ui/SubjectMenuGui.java
+++ src/main/ui/SubjectMenuGui.java
@@ -11,6 +11,7 @@ public class SubjectMenuGui extends JFrame {
 
     private JList list;
     private DefaultListModel listModel;
+    private JPanel mainPanel;
     private JPanel toolBarPane;
 
     private ListOfSubjects listOfSubjects;
@@ -21,24 +22,13 @@ public class SubjectMenuGui extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setPreferredSize(new Dimension(600, 600));
 
-        initializeToolBar();
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+        setContentPane(mainPanel);
 
         init();
-
-        listModel = new DefaultListModel();
-        for (Subject s: listOfSubjects.getListOfSubjects()) {
-            listModel.addElement(s.getName());
-        }
-
-        list = new JList(listModel);
-        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        list.setSelectedIndex(0);
-        list.setLayoutOrientation(JList.VERTICAL);
-        list.setVisibleRowCount(-1);
-
-        JScrollPane listScrollPane = new JScrollPane(list);
-
-        setContentPane(listScrollPane);
+        initializeToolBar();
+        initializeList();
 
         pack();
         setLocationRelativeTo(null);
@@ -49,18 +39,37 @@ public class SubjectMenuGui extends JFrame {
     private void initializeToolBar() {
         toolBarPane = new JPanel();
         toolBarPane.setBorder(new EmptyBorder(2, 2, 2, 2));
-        setContentPane(toolBarPane);
         toolBarPane.setLayout(new FlowLayout());
 
         JButton saveBtn = new JButton("Save");
         JButton loadBtn = new JButton("Load");
         JButton addBtn = new JButton("Add");
         JButton removeBtn = new JButton("Remove");
+        JButton openBtn = new JButton("Open");
 
         toolBarPane.add(saveBtn);
         toolBarPane.add(loadBtn);
         toolBarPane.add(addBtn);
         toolBarPane.add(removeBtn);
+        toolBarPane.add(openBtn);
+        mainPanel.add(toolBarPane, BorderLayout.PAGE_START);
+    }
+
+    private void initializeList() {
+        listModel = new DefaultListModel();
+
+        for (Subject s: listOfSubjects.getListOfSubjects()) {
+            listModel.addElement(s.getName());
+        }
+
+        list = new JList(listModel);
+        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        list.setLayoutOrientation(JList.VERTICAL);
+        list.setVisibleRowCount(-1);
+
+        JScrollPane listScrollPane = new JScrollPane(list);
+
+        mainPanel.add(listScrollPane, BorderLayout.CENTER);
     }
 
     private void init() {
